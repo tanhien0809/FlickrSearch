6b589c18336116500ae23eb0aa3d282ef0d27399
tanhien0809 <hien@Hiens-Mac.local>
2017-04-28 15:07:11 +0700

    Up

--- FlickrSearch/AppDelegate.swift
+++ FlickrSearch/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+let themeColor = UIColor(red: 0.01, green: 0.41, blue: 0.22, alpha: 1.0)
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window?.tintColor = themeColor
         // Override point for customization after application launch.
         return true
     }
---
+++ FlickrSearch/FlickrPhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrSearch
+//
+//  Created by Cntt08 on 4/26/17.
+//  Copyright © 2017 Cntt08. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+}
